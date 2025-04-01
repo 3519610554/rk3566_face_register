235f1f668db4d96e0083b94e783c936cf1c5a603
xuguocai <x3519610554@outlook.com>
2025-04-01 10:19:49 +0800

    🦄 refactor(备份上传):

--- src/main.cpp
+++ src/main.cpp
@@ -23,23 +23,23 @@ int main() {
     //             }
     //         }
     //     }
-    Udp client;
-    std::cout << "begin\n";
-    int state = client.initialize("172.20.10.2", 8080);
-    std::cout << "state: " << state << std::endl;
-    if (!state){
-        while(1){
-            int input_data = 0;
-            std::cin >> input_data;
-            if (input_data==1){
-                int res = client.send_message("Hello");
-                std::cout << "res: " << res << std::endl;
-            }else if (input_data==2){
-                break;
-            }
-        }
-    }
-    client.cleanupNetwork();
+    // Udp client;
+    // std::cout << "begin\n";
+    // int state = client.initialize("172.20.10.2", 8080);
+    // std::cout << "state: " << state << std::endl;
+    // if (!state){
+    //     while(1){
+    //         int input_data = 0;
+    //         std::cin >> input_data;
+    //         if (input_data==1){
+    //             int res = client.send_message("Hello");
+    //             std::cout << "res: " << res << std::endl;
+    //         }else if (input_data==2){
+    //             break;
+    //         }
+    //     }
+    // }
+    // client.cleanupNetwork();
     std::cout << "Hello World\n";
     
     return 0;
